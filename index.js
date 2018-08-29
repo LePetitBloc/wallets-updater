892594f5db8030171b2903c1fa132ed26122daa2
Jules LAURENT <jules.laurent@ynov.com>
2018-08-29 11:06:59 +0200

    add Update class definition

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log(` --wallets updater started at ${new Date()} --`)
+const Update = require('./src/definitions/Update');
+const Version = require('./src/definitions/Version');
+
+console.log(` --wallets updater started at ${new Date()} --`);
